64090058450fd09889f5818ba4341dd792e26291
Jake Meyers <jakejake92@yahoo.com>
2020-10-10 10:06:10 -0600

    progress on API

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,23 @@ const App = () => {
 	};
 
 	useEffect(() => {
-		const response = async fetch("https://api.themoviedb.org/3/movie/343611?api_key=19b7484b16487bbecac75bf455e0c742")
-	
+		const response = async () => {
+			const apiKey = "19b7484b16487bbecac75bf455e0c742";
+			fetch(
+				`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieTerm}`
+			)
+				.then((response) => response.json())
+				.then((data) => console.log(data))
+				.then(
+					fetch(
+						`https://api.themoviedb.org/3/movie/${data.results[0].id}?api_key=${apiKey}&language=en-US`
+					)
+						.then((response) => response.json)
+						.then((data) => console.log(data))
+				);
+		};
+
+		response();
 	}, [movieTerm]);
 
 	return (
